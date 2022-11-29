2c3898a7e818bea868ecf7323761a2124d448752
mikecao <casparcao@aliyun.com>
2022-11-29 15:18:55 +0800

    update

--- ui/lib/constant.dart
+++ ui/lib/constant.dart
@@ -1,8 +1,5 @@
-import 'dart:io';
-
 class Constant {
-  Map<String, String> envs = Platform.environment;
   String baseurl() {
-    return envs["BASE_URL"] ?? "localhost:8080";
+    return "203.196.8.218:8888";
   }
 }
